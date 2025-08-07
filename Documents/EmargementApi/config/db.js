376a981c7e7e776dbf7daf1334f2691f3642d974
Djadide <dmoindandze@myges.fr>
2025-08-07 12:27:23 +0200

    Suppression du dossier Documents/EmargementApi et nettoyage du dépôt

--- Documents/EmargementApi/config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/emargement', { useNewUrlParser: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB', err));
